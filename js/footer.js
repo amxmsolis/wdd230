95bd452be4e719888ea4d7b055b1dd142e5072b8
amxmsolis <amx_msolis@hotmail.com>
2022-04-20 09:23:48 -0500

    with querySelector

--- js/footer.js
+++ js/footer.js
@@ -1,10 +1,2 @@
-const options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric'
-};
-
-document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options);
-document.getElementById('currentTime').textContent = new Date().toLocaleTimeString();
-document.getElementById('currentYear').textContent = new Date().getFullYear();
+document.querySelector('#currentYear').textContent = new Date().getFullYear();
+document.getElementById('currentDate').textContent = new Date(document.lastModified);
