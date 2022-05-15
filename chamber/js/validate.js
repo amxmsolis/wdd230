77e27780ca2a1ac310203ff01adf63b4f852e249
amxmsolis <amx_msolis@hotmail.com>
2022-05-14 22:03:59 -0500

    Chambers20220514

--- chamber/js/validate.js
+++ chamber/js/validate.js
@@ -1,4 +1,14 @@
 /*Monica Solis WDD230 */
+const hambutton = document.querySelector('.menuHamburger');
+const mainnav = document.querySelector('.siteNavigation')
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('changeMenuResp')
+}, false);
+window.onresize = () => {
+    if (window.innerWidth > 760) mainnav.classList.remove('changeMenuResp')
+};
+
 document.querySelector('#currentDate').textContent = new Date().toDateString();
 document.querySelector('#currentYear').textContent = new Date().getFullYear();
 document.getElementById('updateDate').textContent = new Date(document.lastModified).toISOString();
