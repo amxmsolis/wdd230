b2598c912dad610610f2ae068e1a8e28e54d8b08
amxmsolis <amx_msolis@hotmail.com>
2022-05-25 22:04:59 -0500

    Chambers

--- chamber/js/validate.js
+++ chamber/js/validate.js
@@ -13,3 +13,10 @@ const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric
 document.querySelector('#currentDate').textContent = new Date().toLocaleDateString('en-US', options);
 document.querySelector('#currentYear').textContent = new Date().getFullYear();
 document.getElementById('updateDate').textContent = new Date(document.lastModified).toISOString();
+
+
+
+const d = new Date();
+if (( d.getDay()  == 1) ||  ( d.getDay()  == 2)) {
+    document.querySelector('#bannerInfo').style.display="block"; 
+};
