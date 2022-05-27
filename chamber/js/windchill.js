4d0a4d7ca8d9ba9bba3c0659950e0aa6f9390772
amxmsolis <amx_msolis@hotmail.com>
2022-05-26 22:39:07 -0500

    Chambers

---
+++ chamber/js/windchill.js
@@ -0,0 +1,7 @@
+let tempVar= 74;
+let speedVar= 23;
+let windVar= (35.74 + (0.6215 * tempVar))-(35.75 * Math.pow(speedVar,0.16)) + (0.4275*tempVar*Math.pow(speedVar,0.16));
+windVar= Math.round(windVar);
+document.querySelector("#tempVar").textContent= tempVar;
+document.querySelector("#speedVar").textContent= speedVar;
+document.querySelector("#windVar").textContent= windVar;
