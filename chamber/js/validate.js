44880f297f38f520136054a0262d0b94de689da9
amxmsolis <amx_msolis@hotmail.com>
2022-06-04 11:29:49 -0500

    DiscoveryPage

--- chamber/js/validate.js
+++ chamber/js/validate.js
@@ -20,3 +20,32 @@ const d = new Date();
 if (( d.getDay()  == 1) || ( d.getDay()  == 2)) {
     document.querySelector('#bannerInfo').style.display="block"; 
 };
+
+/*20220601 LazyLoad */
+const lazyImages = document.querySelectorAll("[data-src]");
+
+function placeholderImage(img) {
+    const srcImage = img.getAttribute("data-src");
+    if (!srcImage) {
+        return;
+    }
+    img.src = srcImage;
+}
+
+const setsImages = {};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            placeholderImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
+}, setsImages);
+
+lazyImages.forEach(image => {
+    imgObserver.observe(image);
+});
+
