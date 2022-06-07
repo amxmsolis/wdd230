b9dec25d4f890e51aee1f54c59383cc38a413fce
amxmsolis <amx_msolis@hotmail.com>
2022-06-06 20:18:35 -0500

    WK08

--- chamber/js/validate.js
+++ chamber/js/validate.js
@@ -13,6 +13,16 @@ const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric
 document.querySelector('#currentDate').textContent = new Date().toLocaleDateString('en-US', options);
 document.querySelector('#currentYear').textContent = new Date().getFullYear();
 document.getElementById('updateDate').textContent = new Date(document.lastModified).toISOString();
+// 20220604 Local Storage
+const visitsValue = document.querySelector("#lastVisit");
+let numVisits = Number(window.localStorage.getItem("visits"));
+if (numVisits !== 0) {
+    visitsValue.textContent = numVisits;
+} else {
+    visitsValue.textContent = "This is your first visit, welcome!";
+}
+numVisits++;
+localStorage.setItem("visits", numVisits);
 
 const d = new Date();
 if ((d.getDay() == 1) || (d.getDay() == 2)) {
@@ -48,13 +58,3 @@ lazyImages.forEach(image => {
 });
 
 
-// 20220604 Local Storage
-const visitsValue = document.querySelector("#lastVisit");
-let numVisits = Number(window.localStorage.getItem("visits"));
-if (numVisits !== 0) {
-    visitsValue.textContent = numVisits;
-} else {
-    visitsValue.textContent = "Your first timne, welcome!";
-}
-numVisits++;
-localStorage.setItem("visits", numVisits);
