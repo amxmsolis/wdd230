03a5c9d7fb4fc3d05aae810c771908fd59088186
amxmsolis <amx_msolis@hotmail.com>
2022-06-15 21:44:32 -0500

    wk09

--- lesson-7/js/lazyload.js
+++ lesson-7/js/lazyload.js
@@ -1,11 +1,11 @@
 const lazyImages = document.querySelectorAll("[data-src]");
 
 function placeholderImage(img) {
-    const srcImage = img.getAttribute("data-src");
-    if (!srcImage) {
-        return;
-    }
-    img.src = srcImage;
+  const srcImage = img.getAttribute("data-src");
+  if (!srcImage) {
+    return;
+  }
+  img.src = srcImage;
 }
 
 const setsImages = {
@@ -13,16 +13,16 @@ const setsImages = {
 };
 
 const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            placeholderImage(entry.target);
-            imgObserver.unobserve(entry.target); 
-        }
-    })
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      placeholderImage(entry.target);
+      imgObserver.unobserve(entry.target);
+    }
+  })
 }, setsImages);
 
 lazyImages.forEach(image => {
-    imgObserver.observe(image);
+  imgObserver.observe(image);
 });
